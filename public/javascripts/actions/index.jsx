c7a6599f1a12196e695384f19452b526c2f2b223
sunchao <2061857679@qq.com>
2017-12-11 14:10:50 +0800

    添加异步消息中间件

--- public/javascripts/actions/index.jsx
+++ public/javascripts/actions/index.jsx
@@ -1,11 +1,20 @@
 /*分析用户行为*/
 let nextTodoId = 0;
 /*用户添加待办项行为*/
-export const addTodo = (text) => {
+export const addTodo1 = (text) => {
     return {
         type: 'ADD_TODO',
         id: nextTodoId++,
         text
+    }
+};
+export const addTodo = (text) => {
+    //在函数里面可以进行异步操作，从外部获取数据来进行判断是否进行继续进行
+    return (dispatch, getState) => {
+        setTimeout(() => {
+            dispatch(addTodo1(text))
+        }, 2000);
+        //return false;返回false表示函数不进行继续执行，
     }
 };
 /*用户选择展示待办项行为*/
